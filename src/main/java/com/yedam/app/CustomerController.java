3654c46c35c5d4b3e4f5b4a22111ef52acc4128e
MyNameIsYGKim <kelen536@gmail.com>
2023-11-09 10:37:58 +0900

    controller 수정

--- src/main/java/com/yedam/app/CustomerController.java
+++ src/main/java/com/yedam/app/CustomerController.java
@@ -3,6 +3,7 @@ package com.yedam.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yedam.app.accessingdatajpa.Customer;
 import com.yedam.app.accessingdatajpa.CustomerRepository;
@@ -11,6 +12,13 @@ import com.yedam.app.accessingdatajpa.CustomerRepository;
 public class CustomerController {
 	
 	@Autowired CustomerRepository repo;
+	
+	@GetMapping("/")
+	public ModelAndView main() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greet", "<b>안녕</b>하세요");
+		return mv;
+	}
 	
 	@GetMapping("/customer")
 	public Iterable<Customer> findall(){
